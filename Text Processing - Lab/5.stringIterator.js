f410d260dd9a506ca9d0893d74f1f4e3a7578802
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-01-26 19:36:10 +0000

    Add files via upload

---
+++ Text Processing - Lab/5.stringIterator.js
@@ -0,0 +1,22 @@
+function iterator(input) {
+    let words = input.split(" ");
+  let iterator = {
+    next: function () {
+     let isDone = words.length === 0;
+     let currentWord = words.shift();
+
+      return {
+          value: currentWord,
+          done: isDone
+    };
+  }
+}
+let nextElement = iterator.next();
+while(!nextElement.done){
+    console.log(nextElement.value);
+
+    nextElement = iterator.next();
+}
+}
+
+iterator("et couv ugugn utj jsoweu nddejfut");
